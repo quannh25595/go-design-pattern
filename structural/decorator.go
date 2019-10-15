05f4fbcffafb761a64ded5f464eb814d72efc5d4
Quan Ngo Huu <quan.nh.25595@gmail.com>
2019-10-15 13:45:55 +0700

    add decorator

--- structural/decorator.go
+++ structural/decorator.go
@@ -1,1 +1,25 @@
 package structural
+
+import "log"
+
+type Object func(int) int
+
+func LogDecorate(fn Object) Object {
+	return func(n int) int {
+		log.Println("Starting execution with the integer ", n)
+		result := fn(n)
+		log.Println("Result ", result)
+		return result
+	}
+}
+
+//Usage
+
+func Double(n int) int {
+	return n * 2
+}
+
+func decorator() {
+	fn := LogDecorate(Double)
+	fn(5)
+}
