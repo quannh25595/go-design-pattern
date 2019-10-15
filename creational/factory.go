70a2d784f61da3a47a5dc2bdf23c4afe150f6084
Quan Ngo Huu <quan.nh.25595@gmail.com>
2019-10-15 13:38:10 +0700

    add singleton

--- creational/factory.go
+++ creational/factory.go
@@ -1,1 +1,38 @@
 package creational
+
+import "io"
+
+type Store interface {
+	Open(string) (io.ReadWriteCloser, error)
+}
+
+type Disk struct{}
+
+func (s Disk) Open(fileName string) (io.ReadWriteCloser, error) {
+	return nil, nil
+}
+
+type Memory struct{}
+
+func (m Memory) Open(fileName string) (io.ReadWriteCloser, error) {
+	return nil, nil
+}
+
+type StorageType int
+
+const (
+	DiskStorage StorageType = 1 << iota
+	TempStorage
+	MemoryStorage
+)
+
+func NewStore(t StorageType) Store {
+	switch t {
+	case DiskStorage:
+		return Disk{}
+	case MemoryStorage:
+		return Memory{}
+	default:
+		return nil
+	}
+}
--- creational/singleton.go
+++ creational/singleton.go
@@ -1,1 +1,19 @@
 package creational
+
+import "sync"
+
+type singleton map[string]string
+
+var (
+	once     sync.Once
+	instance singleton
+)
+
+func GetInstance() singleton {
+	once.Do(func() {
+		instance = make(singleton)
+	})
+	return instance
+}
+
+// Most of time, singleton reduces the testability
